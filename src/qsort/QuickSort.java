84380c4cbc32c04a7bee5551e95a820ad1437955
archMcMilan <artempryzhkov@gmail.com>
2016-05-29 23:18:23 +0300

    Checked missing

--- src/qsort/QuickSort.java
+++ src/qsort/QuickSort.java
@@ -5,30 +5,35 @@ package qsort;
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] mass = { 1, 3, 4, 9, 2, 6, 5, 8, 7, 0 };
-        mass = qSort(mass,0,mass.length-1);
+        int[] mass = {1, 3, 7, 5, 2, 6, 1, 7, 0};
+        mass = qSort(mass, 0, mass.length - 1);
         print(mass, 0);
     }
-    public static int[] qSort(int[] mas,int start,int finish) {
-        int left=start, right=finish;
-        int op=mas[(left+right)/2];
-        while(left<=right)
-        {
-            while(mas[left]<op)
+
+    public static int[] qSort(int[] mas, int start, int finish) {
+        int left = start, right = finish;
+        int op = mas[(left + right) / 2];
+        System.out.println("op="+op);
+        while (left <= right) {
+            while (mas[left] < op){
                 left++;
-            while(mas[right]>op)
+            }
+            while (mas[right] > op){
                 right--;
-            if(left<=right)
-            {
-                int temp = mas[left++];
-                mas[left++] = mas[right--];
-                mas[right--] = temp;
+
+            }
+            if (left <= right) {
+                int temp = mas[left];
+                mas[left] = mas[right];
+                mas[right] = temp;
+                left++;
+                right--;
             }
         }
-        if(start<right)
-            mas=qSort(mas,start,right);
-        if(finish>left)
-            mas=qSort(mas,left,finish);
+        if (start < right)
+            mas = qSort(mas, start, right);
+        if (finish > left)
+            mas = qSort(mas, left, finish);
         return mas;
     }
 
